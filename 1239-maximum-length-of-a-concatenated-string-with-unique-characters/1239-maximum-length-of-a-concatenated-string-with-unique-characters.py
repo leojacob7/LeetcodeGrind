89f102e5d0bbd807614c40b65cb4b3ddd2906d45
Leo Jacob <33173777+leojacob7@users.noreply.github.com>
2022-10-26 01:34:33 -0400

    Time: 581 ms (18.53%), Space: 14.1 MB (53.80%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+        self.maxSofFar = 0
+
+        def recurse(s, i, arr):
+            # print(">>", i, len(arr), maxSofFar)
+            if i >= len(arr):
+                print(">>", self.maxSofFar)
+                if len(set(s)) == len(s):
+                    self.maxSofFar = max(self.maxSofFar, len(s))
+                    print("here", self.maxSofFar, s)
+                return
+            recurse(s, i+1, arr)
+            if len(set(s+arr[i])) == len(s+arr[i]):
+                recurse(s + arr[i], i+1, arr)
+
+        recurse("", 0, arr)
+        return self.maxSofFar
